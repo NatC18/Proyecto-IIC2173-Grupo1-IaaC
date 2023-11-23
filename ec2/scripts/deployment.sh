d4c47cccdcddcbb4e2997824be2a17e3cd5a54b9
Natalia Correal <natalia.correal@uc.cl>
2023-11-23 13:26:13 -0300

    feat: add terraform files

---
+++ ec2/scripts/deployment.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Update the system
+sudo apt update -y
+sudo apt upgrade -y
+
+# Install Docker APT Repository
+sudo apt install ca-certificates curl gnupg -y
+sudo install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+sudo chmod a+r /etc/apt/keyrings/docker.gpg
+echo \
+"deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+"$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
+sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+# Install Docker
+sudo apt update -y
+sudo apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
